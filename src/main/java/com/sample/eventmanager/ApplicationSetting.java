4dcd0d041cb9e6b15325854e07953e0ddd75f581
ryo_shindo <>
2015-12-02 19:54:07 +0900

    create root package

---
+++ src/main/java/com/sample/eventmanager/ApplicationSetting.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sample.eventmanager;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author ryo_shindo
+ */
+@ApplicationPath("api")
+public class ApplicationSetting extends Application {
+
+    public ApplicationSetting() {
+    }
+    
+}
